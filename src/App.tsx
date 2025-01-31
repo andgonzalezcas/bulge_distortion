b600c154b261602a1814d83391fb67ec28be54b2
Camilo Gonzalez <andgonzalezcas@unal.edu.co>
2025-01-31 16:54:15 -0500

    feat(app): add initial bulge distortion effect on bulge component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-function App() {
+import BulgeImage from "./components/bulgeImage";
 
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
+    <div className="w-screen h-screen flex flex-col">
+      <div className="w-full flex justify-end">
+        <div className="w-[45%] h-[300px]">
+          <BulgeImage />
+        </div>
+      </div>
+      <div className="w-full flex justify-start">
+        <div className="w-[45%] h-[300px]">
+          <BulgeImage />
+        </div>
+      </div>
+      <div className="w-full flex justify-end">
+        <div className="w-[45%] h-[300px]">
+          <BulgeImage />
+        </div>
+      </div>
+      <div className="w-full flex justify-start">
+        <div className="w-[45%] h-[300px]">
+          <BulgeImage />
+        </div>
+      </div>
+      <div className="w-full flex justify-end">
+        <div className="w-[45%] h-[300px]">
+          <BulgeImage />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/bulgeImage.tsx
@@ -0,0 +1,86 @@
+import { useFrame, Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { useAspect, useTexture } from '@react-three/drei';
+import { fragment, vertex } from "../shaders/bulgeMaterial";
+
+//image import
+import imageSrc from "../assets/sala.jpg"
+
+function Mesh({ mouse }: { mouse: number[] }) {
+    const meshRef = useRef<any>(null);
+    const texture = useTexture(imageSrc);
+    const { width, height } = texture.image;
+
+    const scale = useAspect(
+        width,
+        height,
+        1
+    );
+
+    const uniforms = useRef({
+        uTime: { value: 0 },
+        uTexture: { value: texture },
+        uMouse: { value: [0.5, 0.5] },
+        uRadius: { value: 0.95 },
+        uStrength: { value: 1.1 },
+        uBulge: { value: 1 },
+    });
+
+    useFrame(() => {
+        if (!meshRef.current) return;
+        meshRef.current.material.uniforms.uTime.value += 0.04;
+        meshRef.current.material.uniforms.uMouse.value = mouse;
+    });
+
+    return (
+        <mesh ref={meshRef} scale={scale}>
+            <planeGeometry args={[1, 1, 15, 15]} />
+            <shaderMaterial
+                wireframe={false}
+                fragmentShader={fragment}
+                vertexShader={vertex}
+                uniforms={uniforms.current}
+            />
+        </mesh>
+    )
+}
+
+export default function BulgeImage() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [mouse, setMouse] = useState([0.5, 0.5]);
+
+    useEffect(() => {
+        if (!canvasRef.current) return;
+
+        const onMouseMove = (e: MouseEvent) => {
+            if (!canvasRef.current) return;
+
+            const rect = canvasRef.current.getBoundingClientRect();
+
+            const x = (e.clientX - rect.left) / rect.width;
+            const y = 1 - (e.clientY - rect.top) / rect.height;
+
+            if (x >= 0 && x <= 1 && y >= 0 && y <= 1) {
+                setMouse([x, y]);
+            } else {
+                setMouse([0.5, 0.5]);
+            }
+        };
+
+        window.addEventListener("mousemove", onMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", onMouseMove);
+        };
+    }, []);
+
+    return (
+        <Canvas ref={canvasRef} style={{ width: "100%", height: "100%" }}>
+            <ambientLight />
+            <Suspense fallback={null}>
+                <Mesh mouse={mouse} />
+            </Suspense>
+        </Canvas>
+
+    );
+}
